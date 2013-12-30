0312f3b7f47e86f5213cfc50cea784ab7d926504
ken1flan <ken1flan@gmail.com>
2013-12-30 14:57:44 +0900

    最初のコミット

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Inpirations::Application.config.secret_key_base = 'e0d586bfbc469d707586ccdb23ed90e9747f18e7383623a8874562633d2afcdb87bb97d222c1358c00549b29681805e7fada9546333c8fe358a6b6e0c8717683'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Inpirations::Application.config.session_store :cookie_store, key: '_inpirations_session'
