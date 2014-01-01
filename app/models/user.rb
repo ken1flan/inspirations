bc18ccc21715679f38a8c66435495223bb1528d3
ken1flan <ken1flan@gmail.com>
2014-01-01 12:19:33 +0900

    facebook ログイン追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,27 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+  devise :trackable, :omniauthable
+
+  def self.new_with_session(params, session)
+    super.tap do |user|
+      if data = session["devise.facebook_data"] && session["device.facebook_data"]["extra"]["raw_info"]
+        user.email = data["email"]
+      end
+    end
+  end
+
+  def self.find_for_facebook_oauth(auth, sined_in_resource = nil)
+    user = User.where(provider: auth.provider, uid: auth.uid).first
+    unless user
+      # TODO: strong parameter 対応?
+      user = User.create(
+        name: auth.extra.raw_info.name,
+        provider: auth.provider,
+        uid: auth.uid,
+        password: Devise.friendly_token[0, 20]
+      )
+    end
+    user
+  end
 end
